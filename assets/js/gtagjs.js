6d4ceee8d01e8bd04d77940166b6735dffe1cd6e
QuyetMG <quyet@magingam.com>
2023-06-09 14:21:18 +0700

    add gtagjs.js

---
+++ assets/js/gtagjs.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+// Set the gtag using the global variable myGtagId.
+gtag('config', window.myGtagId);
+console.log("myGtagId", window.myGtagId);
